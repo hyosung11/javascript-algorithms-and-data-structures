080e50e775210feeeea14b7c0578d9a15bdfaad3
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-13 22:22:27 -0500

    refactor same.js

--- same.js
+++ same.js
@@ -8,49 +8,49 @@ same([1,2,1], [4,4,1]) // false (must be same frequency)
 
 // A Naive Solution - O(n^2)
 
-function same(arr1, arr2) {
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2)
+//     if (correctIndex === -1) {
+//       return false;
+//     }
+//     console.log(arr2);
+//     arr2.splice(correctIndex, 1)
+//   }
+//   return true;
+// }
+//
+// console.log(same([1,2,3,2], [9,1,4,4]));
+
+
+// Refactored Solution - O(n)
+
+function same2(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2)
-    if (correctIndex === -1) {
-      return false;
-    }
-    console.log(arr2);
-    arr2.splice(correctIndex, 1)
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
   }
-  return true;
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false
+    }
+  }
+  return true
 }
 
-console.log(same([1,2,3,2], [9,1,4,4]));
-
-
-// Refactored Solution
-
-// function same2(arr1, arr2){
-//     if(arr1.length !== arr2.length){
-//         return false;
-//     }
-//     let frequencyCounter1 = {}
-//     let frequencyCounter2 = {}
-//     for(let val of arr1){
-//         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
-//     }
-//     for(let val of arr2){
-//         frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
-//     }
-//     console.log(frequencyCounter1);
-//     console.log(frequencyCounter2);
-//     for(let key in frequencyCounter1){
-//         if(!(key ** 2 in frequencyCounter2)){
-//             return false
-//         }
-//         if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
-//             return false
-//         }
-//     }
-//     return true
-// }
-//
-// sames([1,2,3,2,5], [9,1,4,4,11])
+console.log(same2([1,2,3,2,5], [9,1,4,4,11]));
